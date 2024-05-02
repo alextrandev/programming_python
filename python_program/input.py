ba5a9ece21125ed82df9e0f0c9367e2bc59e2ae7
dk10119 <tranduclong092@gmail.com>
2024-05-02 19:40:50 +0300

    Feat: create a program to take and process user input

---
+++ python_program/input.py
@@ -0,0 +1,44 @@
+"""
+File: input.py
+--------------
+Take 2 input, convert in to integer and return the sum
+"""
+
+def main():
+  prompt = ""
+  while (prompt != "exit"):
+    prompt = input("Type sum for calculate a sum, multiply for a multiply, hello for a greeting and exit to end the program: ")
+    match prompt:
+      case "sum":
+        sum()
+      case "multiply":
+        multiply()
+      case "hello":
+        hello()
+      case "exit":
+        print("Goodbye!")
+      case _:
+        print("Wrong command, try again!")
+
+def sum():
+  print("Calculate the sum of 2 numbers")
+  num1 = input("Enter first number: ")
+  num1 = int(num1)
+  num2 = input("Enter second number: ")
+  num2 = int(num2)
+  total = num1 + num2
+  print(f"Result: {num1} + {num2} = {total}")
+
+def hello():
+  name = input("What is your name? ")
+  print(f"Hello {name}!")
+
+def multiply():
+  print("Calculate the multiply of 2 numbers")
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+  total = num1 * num2
+  print(f"Result: {num1} x {num2} = {total}")
+
+# call the main funtion
+__name__ == '__main__' and main()
