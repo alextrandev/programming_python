93953b60dd387bfee88e1a8eb18256bf55772e59
dk10119 <tranduclong092@gmail.com>
2024-05-02 17:42:37 +0300

    Feat: create first program and add a readme file

---
+++ python_program/helloworld.py
@@ -0,0 +1,11 @@
+"""
+File: helloworld.py
+-------------------
+This is my first python program.
+"""
+
+def main():
+  print("Hello world!")
+
+# call the main funtion
+__name__ == '__main__' and main()
