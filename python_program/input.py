0658a638285977c8ea9a68667229a3be172f403a
dk10119 <tranduclong092@gmail.com>
2024-05-02 22:22:34 +0300

    Feat: add f to c converter

--- python_program/input.py
+++ python_program/input.py
@@ -10,7 +10,7 @@ import random
 def main():
   prompt = ""
   while (prompt != "exit"):
-    prompt = input("Type sum, multiply, hello or dice. Type exit to end the program: ")
+    prompt = input("Type sum, multiply, hello, dice or FtoC. Type exit to end the program: ")
     match prompt:
       case "sum":
         sum()
@@ -20,6 +20,8 @@ def main():
         hello()
       case "dice":
         dice()
+      case "FtoC":
+        f_to_c()
       case "exit":
         print("Goodbye!")
       case _:
@@ -49,6 +51,11 @@ def dice():
   dice = int(input("What is the number of sides on your dice? "))
   roll = random.randint(1, dice)
   print(f"Your roll is {roll}")
+
+def f_to_c():
+    f_temp = float(input("Enter temperature in Fahrenheit: "))
+    c_temp = (f_temp - 32) * 5.0/9.0
+    print(f"Temperature: {f_temp}F = {c_temp}C")
 
 # call the main funtion
 __name__ == '__main__' and main()
