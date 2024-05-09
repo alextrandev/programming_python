7c072be499b5aeb93709450147f1bf3144fe4d41
dk10119 <tranduclong092@gmail.com>
2024-05-09 15:30:14 +0300

    Feat: implement streak count

--- python_program/learn_addition.py
+++ python_program/learn_addition.py
@@ -6,18 +6,29 @@ Basic console game to teach user addition
 
 import random
 
+
 def main():
     print("Welcome to learn addition")
-    num1 = random.randint(10, 99)
-    num2 = random.randint(10, 99)
-    print(f"What is {num1} + {num2}?")
-    sum = int(input("Your answer: "))
 
-    if (sum == num1 + num2):
-        print("Correct!")
-    else:
-        print("Incorrect.")
-        print(f"The expected answer is {num1 + num2}")
+    streak = 0
+    while (streak < 3):
+      num1 = random.randint(10, 99)
+      num2 = random.randint(10, 99)
+      print(f"What is {num1} + {num2}?")
+      sum = int(input("Your answer: "))
+
+      if (sum == num1 + num2):
+          streak += 1
+          print("Correct!")
+          print(f"You've gotten {streak} correct in a row")
+      else:
+          streak = 0
+          print("Incorrect.")
+          print(f"The expected answer is {num1 + num2}")
+
+    print("Congratulations! You mastered addition.")
+      
+
     
     
 __name__ == '__main__' and main()
