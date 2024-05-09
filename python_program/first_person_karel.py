1fb0abea2182094e65933d53e3c285ea85e13e03
dk10119 <tranduclong092@gmail.com>
2024-05-09 18:02:20 +0300

    Feat: add turn left function

--- python_program/first_person_karel.py
+++ python_program/first_person_karel.py
@@ -8,38 +8,46 @@ N_COLS = 3
 N_ROWS = 3
 
 def main():
-    cur_row = 1
-    cur_col = 1
-    cur_dir = "East"
-    print(f"Welcome to first person Karel! You're at row {cur_row}, column {cur_col}, facing {cur_dir} (facing column 3)")
+  cur_row = 1
+  cur_col = 1
+  cur_dir = "East"
+  
+  print(f"Welcome to first person Karel! You're at row {cur_row}, column {cur_col}, facing {cur_dir} (facing column 3)")
+  user_input = input("What would you like to do? You can move and turn left. Press enter to finish. ")
+
+  while user_input != "":
+    if user_input == "move":
+      match cur_dir:
+        case "East" if cur_col < N_COLS:
+          cur_col += 1
+          print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
+        case "West" if cur_col > 1:
+          cur_col -= 1
+          print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
+        case "North" if cur_row < N_ROWS:
+          cur_row += 1
+          print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
+        case "South" if cur_row > 1:
+          cur_row -= 1
+          print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
+        case _:
+          print("You can't move forward!")
+    elif user_input == "turn left":
+      match cur_dir:
+        case "East":
+          cur_dir = "North"
+        case "West":
+          cur_dir = "South"
+        case "North":
+          cur_dir = "West"
+        case "South":
+          cur_dir = "East"
+      print(f"You turned left and are now facing {cur_dir}.")
+    else:
+      print("Wrong command. Try 'move' or 'turn left'")
+
     user_input = input("What would you like to do? You can move and turn left. Press enter to finish. ")
-    while user_input != "":
-        if user_input == "move":
-            match cur_dir:
-                case "East" if cur_col < N_COLS:
-                    cur_col += 1
-                    print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
-                case "West" if cur_col > 1:
-                    cur_col -= 1
-                    print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
-                case "North" if cur_row < N_ROWS:
-                    cur_row += 1
-                    print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
-                case "South" if cur_row > 1:
-                    cur_row -= 1
-                    print(f"You moved one step forward and now you're at row {cur_row} column {cur_col}.")
-                case _:
-                    print("You can't move forward!")
 
-        elif user_input == "turn left":
-            pass
-        else:
-            print("Wrong command. Try 'move' or 'turn left'")
+  print(f"You've ended up at row {cur_row} and column {cur_col} facing {cur_dir}.")
 
-        user_input = input("What would you like to do? You can move and turn left. Press enter to finish. ")
-    
-    print(f"You've ended up at row {cur_row} and column {cur_col} facing {cur_dir}.")
-
-
-if __name__ == '__main__':
-    main()
+__name__ == '__main__' and main()
