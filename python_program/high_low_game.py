379670d27d784d61abff59ed32c6a0876b86da29
dk10119 <tranduclong092@gmail.com>
2024-05-16 18:10:44 +0300

    Feat: get user input and game logic

--- python_program/high_low_game.py
+++ python_program/high_low_game.py
@@ -8,6 +8,8 @@ The high low game goes as follows:
 These steps make up one round of the game. The game is over after all rounds have been played.
 
 Step 1: Generate the random numbers, print it out to test logic on later steps
+Step 2: Get user choice
+Step 3: Write the game logic
 """
 
 import random
@@ -20,9 +22,18 @@ def main():
 
     player_num = random.randint(1, 100)
     computer_num = random.randint(1, 100)
-
     print(f"Your number is {player_num}")
     print(f"Computer's number is {computer_num}")
+
+    choice = input("Do you think your number is higher or lower than the computer's?: ")
+
+    if (choice == "lower" and player_num < computer_num 
+        or choice == "higher" and player_num > computer_num):
+        print(f"You were right! The computer's number was {computer_num}") 
+    elif (choice == "lower" or choice == "higher"):
+        print(f"Aww, that's incorrect. The computer's number was {computer_num}")
+    else:
+        print(f"Wrong input. Please type 'higher' or 'lower'")
 
     print("Thanks for playing!")
 
