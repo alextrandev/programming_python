2160b52a0c29302b435bf8382d0f96ea8cfc16be
Alex Tran <tranduclong092@gmail.com>
2024-06-10 17:48:59 +0300

    Feat: create a pyramid program

---
+++ python_graphic/draw_pyramid.py
@@ -0,0 +1,21 @@
+"""
+File: draw_pyramid.py
+---------------------------
+Draw a perfect pyramid with pre-defined brick size. Each higer row have 1 less brick and the pyramid should be centered on the canvas
+"""
+
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 600      # Width of drawing canvas in pixels
+CANVAS_HEIGHT = 300     # Height of drawing canvas in pixels
+
+BRICK_WIDTH	= 30        # The width of each brick in pixels
+BRICK_HEIGHT = 12       # The height of each brick in pixels
+BRICKS_IN_BASE = 14     # The number of bricks in the base
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)  
+
+if __name__ == '__main__':
+    main()
