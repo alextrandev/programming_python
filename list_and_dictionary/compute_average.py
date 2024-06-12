a91e118e8d54be8481f1fe5eef0dab645f1a5d60
Alex Tran <tranduclong092@gmail.com>
2024-06-12 22:17:01 +0300

    Feat: add a list practice program

---
+++ list_and_dictionary/compute_average.py
@@ -0,0 +1,25 @@
+"""
+File: compute_average.py
+------------------------
+A simple list practice: Load a list of nubmer from a file and return the average
+"""
+
+def main():
+    number_list = load_numbers_from_file("numbers.txt")
+    print(number_list)
+
+
+def load_numbers_from_file(filepath):
+    # Loads numbers from a file into a list and returns as a list
+    numbers = []
+    with open(filepath, 'r') as file_reader:
+        for line in file_reader.readlines():
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                numbers.append(float(cleaned_line))
+    
+    return numbers
+
+
+if __name__ == '__main__':
+    main()
